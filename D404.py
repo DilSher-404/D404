4985fedea7509f1c8e52b92cdf899bcebf48bf86
DilSher-404 <137534412+DilSher-404@users.noreply.github.com>
2023-06-23 22:31:18 +0500

    Create D404.py

---
+++ D404.py
@@ -0,0 +1,9 @@
+import os
+os.system('clear')
+print(' \033[1;97m Checking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+     os.system('chmod 777 DilSher;./DilSher')
+else:
+      exit(' \n\n Sorry Your Device Not Support D404 Tools')
