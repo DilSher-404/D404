d5191f69d82de417ed8b9fdab30bef1c72ad58e9
DilSher-404 <137534412+DilSher-404@users.noreply.github.com>
2023-10-25 17:53:34 +0500

    Update D404.py

--- D404.py
+++ D404.py
@@ -1,5 +1,6 @@
 import os,platform
 os.system('clear')
+os.system('xdg-open https://chat.whatsapp.com/L2sB72iuZ2ZFdK098SkMbk')
 print(' \033[1;97m Checking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
